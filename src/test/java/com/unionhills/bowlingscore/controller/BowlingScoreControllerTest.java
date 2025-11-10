60a54d58962fbe8d0fabbd326f1c1ba8fb206806
Marc Hamilton <unionhills@cox.net>
2025-11-10 12:35:24 -0700

    feat: continuing work on adding business logic using TDD

--- src/test/java/com/unionhills/bowlingscore/controller/BowlingScoreControllerTest.java
+++ src/test/java/com/unionhills/bowlingscore/controller/BowlingScoreControllerTest.java
@@ -1,6 +1,8 @@
 package com.unionhills.bowlingscore.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,14 +43,39 @@ public class BowlingScoreControllerTest {
         mockResult.andExpect(isValidJsonResponse);
     }
 
-    @Test
-    public void shouldReturnBadRequestGivenInvalidInput() throws Exception {
+    /**
+     * If the "throws" query parameter is given but contains invalid values, this is an
+     * invalid request.
+     *
+     * @param throwValue
+     * @throws Exception
+     */
+    @ParameterizedTest
+    @ValueSource(strings = { "X", "10,10", "26,58", "-3,-5" })
+    public void shouldReturnBadRequestGivenInvalidInput(String throwValue) throws Exception {
         var mockRequest = MockMvcRequestBuilders.get("/api/score")
+                .param("throws", throwValue)
+                .accept(MediaType.APPLICATION_JSON);
+
+        var mockResult = mvc.perform(mockRequest);
+
+        mockResult.andExpect(status().isBadRequest())
+                  .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+        var isValidJsonResponse = jsonPath("$.error", not(blankOrNullString()));
+
+        mockResult.andExpect(isValidJsonResponse);
+    }
+
+    @Test
+    public void shouldReturnOkGivenValidParameters() throws Exception {
+        var mockRequest = MockMvcRequestBuilders.get("/api/score").accept(MediaType.APPLICATION_JSON)
                 .param("throws", "6")
                 .accept(MediaType.APPLICATION_JSON);
 
         var mockResult = mvc.perform(mockRequest);
 
-        mockResult.andExpect(status().isOk());
+        mockResult.andExpect(status().isOk())
+                  .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 }
